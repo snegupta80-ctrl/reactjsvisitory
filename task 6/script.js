9de06186d56a3134428f52655e5949579d784cb5
snegupta80-ctrl <snegupta80@gmail.com>
2026-04-16 15:17:18 +0530

    REACTJSVISITORY

---
+++ task 6/script.js
@@ -0,0 +1,11 @@
+document.getElementById('get-btn').addEventListener('click', getData);
+
+async function getData() {
+    try {
+        let response = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
+        let data = await response.json();
+        document.getElementById('status').innerText = data.rates.INR;
+    } catch (error) {
+        console.log(error);
+    }
+}
